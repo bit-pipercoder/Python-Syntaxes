421c97ac7dd2f79576a98b62615bb604b2f1ade0
bit piper <madonnacadampog@proton.me>
2026-09-03 21:23:38 +0800

    Update pythonsyntaxes.py

--- pythonsyntaxes.py
+++ pythonsyntaxes.py
@@ -19,5 +19,23 @@ print("What is your name? " + name) # prints What is your name and Bernard to th
 example = input("Bernard") # it allows you to type in the output that is what range() is for
 
 print(len(name)) # prints the length of the name variable, so it can be either 1 or 7 due to bernard beinf 7 characters or 1 word
+test = {
+    "Test"
+} # MUST be 4 spaces after "Test" or else a indentation error comes up and this is called "Dictionary" contains multiple text in another text
+ # fun fact concatenation is useful you can use it like
+Nerds = "Your long paragraph here" # then you can concatenate nerds to another line so it prints whats inside nerds
+
+print("Your print thing here on first " + Nerds)
+
+# LUAU SYNTAXES
+if # checks if a thing exists usually paired with then
+then # continues on with the script WHEN the if statement is done
+-- test # comment
+print("") # print, you already got this before if you read the print on top
+print() # prints a thing in the explorer, OR prints a variable.
+for i, v in ipairs(yourTableName) do # reads the entire table
+print(v) # prints the value inside i, v (v) which prints the table instead of a table identifier
+end # required for the for statement
+while true do # not recommended without a task.wait()
 
 # thats all i know for python, streaming my python journey on youtube.com
