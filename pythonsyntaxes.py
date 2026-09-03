b33a05bb85c843a08765205a5263206e6ab93ea9
bit piper <madonnacadampog@proton.me>
2026-09-03 21:18:13 +0800

    Create pythonsyntaxes.py

---
+++ pythonsyntaxes.py
@@ -0,0 +1,23 @@
+# all of the syntaxes here are examples
+
+print("Hello, World!") # prints hello to the output
+
+# next syntaxes
+
+name = "bernard" # create a variable called name so you can use it in prints
+
+# print("") prints something to a output print() prints a variable length, of something to the output
+print(name) # prints the variable name to the output and name contains bernard so it prints bernard
+
+# concatenation
+# concatenation is + from a string to a variable
+# for example print("What is your name? " + name)
+
+print("What is your name? " + name) # prints What is your name and Bernard to the output fully saying What is your name? Bernard
+
+# for my python codepad this doesnt work
+example = input("Bernard") # it allows you to type in the output that is what range() is for
+
+print(len(name)) # prints the length of the name variable, so it can be either 1 or 7 due to bernard beinf 7 characters or 1 word
+
+# thats all i know for python, streaming my python journey on youtube.com
