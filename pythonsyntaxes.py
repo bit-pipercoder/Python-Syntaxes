935a5d266d8aa9d2f825d134f01d0bf446791180
bit piper <madonnacadampog@proton.me>
2026-09-07 19:48:09 +0800

    Update pythonsyntaxes.py

--- pythonsyntaxes.py
+++ pythonsyntaxes.py
@@ -1,48 +1,97 @@
 # all of the syntaxes here are examples
 
-print("Hello, World!") # prints hello to the output
+print(“Hello, World!”) # prints hello, world to the output
 
 # next syntaxes
 
-name = "bernard" # create a variable called name so you can use it in prints
+name = “bernard” # creates a variable called name so you can use it in prints
 
-# print("") prints something to a output print() prints a variable length, of something to the output
-print(name) # prints the variable name to the output and name contains bernard so it prints bernard
+print(””) prints something to the output
+
+print() can also print a variable, number, etc.
+
+print(name) # prints the variable name to the output
+
+# name contains bernard so it prints bernard
 
 # concatenation
-# concatenation is + from a string to a variable
-# for example print("What is your name? " + name)
 
-print("What is your name? " + name) # prints What is your name and Bernard to the output fully saying What is your name? Bernard
+# concatenation is using + to connect a string to a variable
 
-# for my python codepad this doesnt work
-example = input("Bernard") # it allows you to type in the output that is what range() is for
+# for example:
 
-print(len(name)) # prints the length of the name variable, so it can be either 1 or 7 due to bernard being 7 characters or 1 word
+print(“What is your name? “ + name)
+
+print(“What is your name? “ + name) # prints What is your name? Bernard
+
+example = input(“Enter something: “) # allows you to type something in the output
+
+print(len(name)) # prints the length of the name variable
+
+# bernard has 7 characters so this prints 7
+
+# sets
+
 test = {
-    "Test"
-} # MUST be 4 spaces after "Test" or else a indentation error comes up and this is called "Dictionary" contains multiple text in another text
- # fun fact concatenation is useful you can use it like
-Nerds = "Your long paragraph here" # then you can concatenate nerds to another line so it prints whats inside nerds
+“Test”
+} # this is a set, not a dictionary
 
-print("Your print thing here on first " + Nerds)
+# dictionaries
 
-import # imports something, you can use this to import libraries and built in 
-# thats all i know for python, streaming my python journey on youtube.com
+example = {
+“name”: “Bernard”,
+“language”: “Python”
+} # dictionaries store things using keys and values
 
+you can print something from the dictionary like this:
+
+print(example[“name”]) # prints Bernard
+
+# fun fact: concatenation is useful
+
+# you can use it with a variable containing a long paragraph
+
+Nerds = “Your long paragraph here”
+
+print(“Your print thing here on first “ + Nerds)
+
+# import
+
+import math # imports a module/library
+
+# you can use import to import built-in or external libraries
+
+# range
+
+range(0, 10) # creates a range from 0 to 9
+
+# the 10 isnt included
+
+# thats all i know for python so far
+
+# streaming my python journey on youtube.com
 
 # PROPER ARRANGEMENT
-print() # prints a variable or dictionary, etc.
-print("") # prints something to the output
-range(-yourNumber, yourNumber) # makes a range between yournumber to yournumber
-variableName = yourVariableThing # creats a variable containing something
-import # you can use this to import libraries for websites use django and flask for gui use tkinter
-input("") # checks if the person typed something in the output
-example = { # dictionary so you dont make variables like variable1, 2 etc.
-    "Example" # must be 4 spaces in the back of the example or else it will give a indentation error
-    "Test" # second thing
-} # closes the dictionary
-+ # concatenation use it to concatenate a variable, smth short for concatenate is concat
-len(something) # prints the length of something mostly used in print("") or print()
 
+print() # prints a blank line
+print(””) # prints something to the output
+range(0, 10) # makes a range from 0 to 9
+variableName = yourVariableThing # creates a variable containing something
+input(””) # allows the person to type something
+len(something) # gets the length of something
+import math # imports a library/module
 
+# dictionary
+
+example = {
+“Example”: “Test”
+} # dictionary with a key and value
+
+# sets
+
+example = {
+“Example”,
+“Test”
+} # set containing multiple values
+
+# thats it for now
